4707355ec7605be3cb1af4d8b762563486b1f6e6
iiandmy <i.budzevichx@dodobrands.io>
2026-07-29 12:34:27 +0300

    fix: validation pipe

--- apps/api/src/modules/transfers/transfers.controller.ts
+++ apps/api/src/modules/transfers/transfers.controller.ts
@@ -1,6 +1,6 @@
 import type { Buffer } from 'node:buffer';
 import type { UploadedFile } from '../files/interfaces/transfer-file-response.interface';
-import type { QueryTransfersDto } from './dto/query-transfers.dto';
+import { QueryTransfersDto } from './dto/query-transfers.dto';
 import type { PaginatedTransfersResponse, TransferResponse } from './interfaces/transfer-response.interface';
 import type { TransfersService } from './transfers.service';
 import {
